a9d24561dbbc1dbae1250c94f8dc98c2472a9679
Omegaclarizamanuela <megaclrsa@gmail.com>
2025-03-25 23:46:24 +0700

    menambah view dan controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
+  Selamat datang di home {{ $nama }}<br>
+  Anda adalah seorang {{ $pekerjaan }}
+
+</div>
---
+++ resources/views/index_view.blade.php
@@ -0,0 +1,42 @@
+<div>   
+<!Doctype html>
+<html lang="en">
+    <title>Serenity Trails</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&amp;family=Great+Vibes&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&amp;family=Great+Vibes&amp;display=swap"rel="stylesheet" />
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-image: url(img/Home.jpg);
+            background-size: cover;
+            background-position: center;
+        }
+
+
+    </style>
+    <div class="header">
+        <?php
+
+        include('header.php');
+
+        ?>
+    <div>
+</head>
+<div class="container mx-auto relative z-10 text-center py-32">
+    <table>
+        <thead>
+            <tr>
+                <td h1 class="text-6xl font-bold font-roboto" style="color: black; ">Temukan keindahan Dan Kedamaian Di Berbagai Destinasi Religi Di Kota Batam</td>
+            </tr>
+            <tr>
+                <td p class="mt-4 text-xl font-great-vibes" style="color: black">Official Team Serenity Trails</td>
+            </tr>
+        </thead>
+    </table>
+</div>
+
+</div>
